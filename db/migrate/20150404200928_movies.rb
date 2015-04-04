294d93da98a20244c81370551706f537ce77e6d7
Micah Bell <redfroglet@gmail.com>
2015-04-04 21:46:20 +0000

    This

--- db/migrate/20150404200928_movies.rb
+++ db/migrate/20150404200928_movies.rb
@@ -1,6 +1,6 @@
 class Movies < ActiveRecord::Migration
   def change
-    create_table :products
+    create_table :movies
         t.string :title, :limit => 50, :null=>false
         t.string :format, :null=>false
         t.integer :length, unsigned: true, :limit => 2,null:=>false
