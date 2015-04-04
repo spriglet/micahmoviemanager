9bf21fdeb092ada0534bcfe5268a7b55d2416503
Micah Bell <redfroglet@gmail.com>
2015-04-04 22:40:32 +0000

    Commit

--- db/migrate/20150404200928_movies.rb
+++ db/migrate/20150404200928_movies.rb
@@ -1,12 +1,12 @@
 class Movies < ActiveRecord::Migration
   def change
     create_table :movies do |t|
-        t.string :title, :limit => 50, :null:false
+        t.string :title, :limit => 50, null:false
         t.string :format, :null=>false
         t.integer :length, unsigned: true, :limit => 2,null:false
         t.integer :release_year, unsigned: true, :limit =>2,null:false
         t.integer :rating, unsigned: true, :limit =>1,null:false
-        t.timestamps null:false
+        t.timestamps null: false
     end
   end
 end
