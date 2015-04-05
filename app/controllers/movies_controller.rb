b288cae43b2a2ec7aca209aafe816a5d86a18170
Micah Bell <redfroglet@gmail.com>
2015-04-05 20:27:13 +0000

    Commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,16 @@
 class MoviesController < ApplicationController
+  # adds a new movie to the collection and displays
+  def create
+  end
+  #reads a movie 
+  def read
+    
+  end
+  #updates a movie
+  def update
+  end
+  #list a movie collection 
+  def list
+    
+  end
 end
--- app/controllers/pages_controller.rb
+++
@@ -1,6 +0,0 @@
-
-class PagesController < ApplicationController
-  def home
-    puts "Honey, I'm Home!"
-  end
-end
