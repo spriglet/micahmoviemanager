9ed1a1d2bdf84956d652c9c1e6c9785372136823
Micah Bell <redfroglet@gmail.com>
2015-04-07 23:30:05 +0000

    Commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,7 +10,7 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.new(movie_params)
     @movie.save
-    redirect_to @home
+    redirect_to :back
   end
   private
   def movie_params
