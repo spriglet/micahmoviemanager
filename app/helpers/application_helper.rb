ed02ca021248ac4c249df774cc7e8bc478dd5f7d
Micah Bell <redfroglet@gmail.com>
2015-04-09 20:44:55 +0000

    Commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,3 +5,9 @@ module ApplicationHelper
     link_to title, :sort => column, :direction => direction
   end
 end
+def nav_link_to(link_text, link_path)
+  class_name = current_page?(link_path) ? 'active' : ''
+  content_tag(:li, :class => class_name) do
+    link_to link_text, link_path
+  end
+end
